73db3d3013da56de3400a1e40426857e843d8210
kikok <s1090374@gm.pu.edu.tw>
2023-10-22 15:23:32 +0800

    加入專案檔案。

---
+++ HelloApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            double num1 = 25;
+            double num2 = 8;
+
+            double result = num1 / num2;
+            result = Math.Round(result, 2);
+
+                Console.WriteLine(result);
+            }
+        }
+    }
